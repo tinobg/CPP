acf3c1f6a784f4e8d61072e23bb25ead41cb0274
Brighton Joves <145933968+tinobg@users.noreply.github.com>
2024-12-20 08:26:17 +0100

    Delete cpp01 directory

--- cpp01/ex03/HumanA.cpp
+++
@@ -1,9 +0,0 @@
-#include "HumanA.hpp"
-#include <iostream>
-
-HumanA::HumanA(std::string name, Weapon& weapon) : name(name), weapon(weapon) {}
-
-void HumanA::attack() const {
-	std::cout << name << " attack with their " << weapon.getType() << std::endl;
-	
-}
